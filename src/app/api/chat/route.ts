e4008661e6b720120edf45a960987a65cc1e4e46
Tom Watkins <46263076+tomwatkins1994@users.noreply.github.com>
2024-07-10 12:56:54 +0100

    Added type

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -20,7 +20,7 @@ export async function POST(req: NextRequest): Promise<Response> {
         );
     }
 
-    function sleep(ms) {
+    function sleep(ms: number) {
         return new Promise(resolve => setTimeout(resolve, ms));
     }
 
